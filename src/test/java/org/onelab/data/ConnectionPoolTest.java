3f7543b5bb48dec91e48278875f885878f4184e7
hanchunliang <hanchunliang@tftiancai.com>
2018-05-16 11:48:37 +0800

    性能测试

--- src/test/java/org/onelab/data/ConnectionPoolTest.java
+++ src/test/java/org/onelab/data/ConnectionPoolTest.java
@@ -8,46 +8,50 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Chunliang.Han on 15/8/15.
  */
 public class ConnectionPoolTest {
-  public static void main(String[] args){
+  public static void main(String[] args) throws InterruptedException {
     Config config = new Config();
     config.setDriver("com.mysql.jdbc.Driver");
     config.setUrl("jdbc:mysql://192.168.0.249:3306/Biz?useUnicode=true&amp;characterEncoding=UTF-8");
     config.setUser("root");
     config.setPassword("root");
-    config.setMinPoolSize(5);
-    config.setMaxPoolSize(15);
+    config.setMinPoolSize(10);
+    config.setMaxPoolSize(40);
     config.setInvalidTime(1);
     final ConnectionPool connectionPool = new ConnectionPool(config);
     final Set<Connection> connectionSet = new HashSet<Connection>();
-    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    final long lastTime = System.currentTimeMillis()/1000;
+    ExecutorService executorService = Executors.newFixedThreadPool(1000);
     Runnable runnable = new Runnable() {
+      AtomicInteger printTime = new AtomicInteger();
+      AtomicLong count = new AtomicLong(0);
       public void run() {
-        Connection connection = connectionPool.getConnection();
-        connectionSet.add(connection);
-        int size = connectionSet.size();
-        int pullSize = connectionPool.size();
-        System.out.println("total source:" + size + " pool size:" + pullSize);
-        connectionPool.close();
+        try {
+          Connection connection = connectionPool.getConnection();
+          connectionSet.add(connection);
+          connectionPool.close();
+          count.addAndGet(1);
+          if (System.currentTimeMillis()/1000-lastTime >= printTime.intValue()){
+            printTime.addAndGet(1);
+            System.out.println("total:"+count.longValue()+
+                               " tps:"+count.longValue()/(System.currentTimeMillis()/1000-lastTime)+
+                               " source:" + connectionSet.size() +
+                               " pool size:" + connectionPool.size());
+          }
+        } catch (Throwable t){
+          t.printStackTrace();
+        }
       }
     };
-    new ConnectionPool(config).getConnection();
-    System.out.print(22);
-    new ConnectionPool(config).getConnection();
-    System.out.print(22);
-//    while (true){
-//      executorService.execute(runnable);
-//    }
-    executorService.execute(runnable);
-    try {
-      Thread.sleep(100000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    while (true){
+//      Thread.sleep(1);
+      executorService.execute(runnable);
     }
-    executorService.shutdown();
   }
 }
